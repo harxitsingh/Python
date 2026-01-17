6e705c7984593e23f256611de7d8f0b9a1362359
Harshit Singh <146829494+harxitsingh@users.noreply.github.com>
2026-01-17 22:00:41 +0530

    Add files via upload

---
+++ python class/1.py
@@ -0,0 +1,41 @@
+# #  Basic Assignment
+# num3 =5 
+# num3*=4
+# print(num3)
+
+# # Assign a value to a variable and print it.
+# num2 = 12
+# print(num2)
+
+# # Conditonal Statements
+
+# # write a program to check whether a no. is postive , neg or zero 
+# num = int(input("Enter your number :"))
+# if num > 0 :
+#  print("postive")
+# else :
+#  if num <0:
+#   print("Negative")
+#  else :
+#   print("Zero")
+  
+#   #write a program to display whether a given number is single digit, double digit or more than two digits.
+#   num = int(input("Enter your number :"))
+#   if -9<= num <= 9
+#    print ("one digit")
+
+
+# list operations
+# Print 1 st element , last and length of list 
+num_list = [1,2,3,4,5]
+print(num_list[0])
+print(num_list[-1])
+print(len(num_list))
+
+#find sum and average using built in functions 
+total = sum(num_list)
+avg = total/ len(num_list)
+print(avg)
+
+#
+fruits = [apple , bnana ,grapes ]
