46c69ef642a51a996e54f9d518ebd956b08933d3
Harshit Singh <harxitsingh@gmail.com>
2026-01-23 16:50:05 +0530

    Add math module practice programs
    Added Python programs for math module:
    - sqrt, factorial, floor, ceil
    - power, absolute value
    - sine, cosine, tangent (with radians)
    - gcd, exp, log10, radians conversion
    
    Code tested and working.

---
+++ modules.py
@@ -0,0 +1,42 @@
+# Importing Modules 
+
+#1. square root of 625
+import math
+print(math.sqrt(625))           #output - 25.0
+
+
+#2. Factorial of 6
+print(math.factorial(6))         #output - 720
+
+#3. floor and Ceiling values of 4.7
+print(math.floor(4.7))             # output - 4
+print(math.ceil(4.7))             #output - 5
+
+#4. 5 raised to power of 3
+print(math.pow(5,3))             #output - 125.0
+
+#5 absolute value of -18 
+print(math.fabs(-18))  #output - 18
+
+#6 sine , cosine ,tangent of 90 degree 
+# converts degree into radians first 
+angle = math.radians(90)
+print(math.sin(angle))  
+print(math.cos(angle))
+print(math.tan(angle))
+#output - 1.0
+# 6.123233995736766e-17
+# 1.633123935319537e+16
+
+#7 .  GCD of 36 and 60
+print(math.gcd(36,60)) # gcd is greatest common divisor - 12
+
+#8 value of e^3
+print(math.exp(3)) # output - 20.085536923187668
+
+#9 log base 10 of 1000
+print(math.log10(1000)) #output - 3.0
+
+#10. convert 180 degree into radians
+print(math.radians(180))   #output - 3.141592653589793
+
